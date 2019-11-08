6daaace8b389782365ad0d07edcc2ddfc5f869c4
wcy <2473925817@qq.com>
2019-11-08 18:22:56 +0800

    change

--- src/main/java/com/vesystem/CasClientApplication.java
+++ src/main/java/com/vesystem/CasClientApplication.java
@@ -18,19 +18,12 @@ public class CasClientApplication extends CasClientConfigurerAdapter{
 	@Value("${cas-service-redirect-url}")
 	private String serviceRedirectUrl;
 
-	@Value("${cas-ignore-pattern}")
-	private String ignorePatter;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CasClientApplication.class, args);
 	}
 
 	@Override
 	public void configureValidationFilter(FilterRegistrationBean validationFilter) {
-		/*List<String> urlPatterns = new ArrayList<>();
-		urlPatterns.add("/cas/login");
-		urlPatterns.add("/index");
-		validationFilter.setUrlPatterns(urlPatterns);*/
 		Map<String, String> map = validationFilter.getInitParameters();
 		map.remove("serverName");
 		map.put("service",serviceRedirectUrl);
@@ -38,12 +31,9 @@ public class CasClientApplication extends CasClientConfigurerAdapter{
 
 	@Override
 	public void configureAuthenticationFilter(FilterRegistrationBean authenticationFilter) {
-		/*List<String> urlPatterns = new ArrayList<>();
-		urlPatterns.add("/cas*//**//*");
-		authenticationFilter.setUrlPatterns(urlPatterns);*/
 		Map<String, String> map = authenticationFilter.getInitParameters();
 		map.remove("serverName");
 		map.put("service",serviceRedirectUrl);
-		//map.put("ignorePattern",ignorePatter);
+		//map.put("ignorePattern","/login|/static/*");
 	}
 }
--- src/main/java/com/vesystem/controller/LoginController.java
+++ src/main/java/com/vesystem/controller/LoginController.java
@@ -19,7 +19,11 @@ import java.util.Map;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(){
+    public String login(HttpServletRequest request){
+        Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
+        if(assertion != null){
+            return "forward:/index";
+        }
         return "login";
     }
 
@@ -34,9 +38,9 @@ public class LoginController {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
 
-        System.out.println("ValidFromDate"+simpleDateFormat.format(assertion.getValidFromDate()));
+      //  System.out.println("ValidFromDate"+simpleDateFormat.format(assertion.getValidFromDate()));
       //  System.out.println("alidUntilDate"+simpleDateFormat.format(assertion.getValidUntilDate()));
-        System.out.println("AuthenticationDate"+simpleDateFormat.format(assertion.getAuthenticationDate()));
+      //  System.out.println("AuthenticationDate"+simpleDateFormat.format(assertion.getAuthenticationDate()));
 
         System.out.println("---------------------------------------------------------------------------");
         AttributePrincipal principal = assertion.getPrincipal();
@@ -56,7 +60,7 @@ public class LoginController {
     @RequestMapping("/logout")
     public String loginOut(HttpSession session) {
         session.invalidate();
-        //退出登录后，跳转到退成成功的页面，不走默认页面
-        return "redirect:http://192.168.50.156:8080/cas/logout";
+        //退出登录后，跳转到退成成功的页面
+        return "redirect:http://192.168.50.156:8080/cas/logout?service=http://192.168.50.156:9090/login";
     }
     }
